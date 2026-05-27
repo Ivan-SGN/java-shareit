6df34cf7b782e2b9435603d3d8719dcf5fa58cc6
IvanMakarov3301 <ivanmakarovxy@gmail.com>
2026-05-27 22:10:41 +0300

    Вынести приложение в отдельный модуль

---
+++ server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -0,0 +1,63 @@
+
+package ru.practicum.shareit.request;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.request.dto.ItemRequestCreateDto;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.service.ItemRequestService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping(path = "/requests")
+@RequiredArgsConstructor
+@Slf4j
+public class ItemRequestController {
+
+    private static final String USER_ID_HEADER = "X-Sharer-User-Id";
+
+    private final ItemRequestService itemRequestService;
+
+    @PostMapping
+    public ItemRequestDto create(
+            @RequestHeader(USER_ID_HEADER) Long userId,
+            @Valid @RequestBody ItemRequestCreateDto dto
+    ) {
+        log.info("Create request userId={}", userId);
+        return itemRequestService.create(userId, dto);
+    }
+
+    @GetMapping
+    public Collection<ItemRequestDto> getOwnRequests(
+            @RequestHeader(USER_ID_HEADER) Long userId
+    ) {
+        log.info("Get own requests userId={}", userId);
+        return itemRequestService.getOwnRequests(userId);
+    }
+
+    @GetMapping("/all")
+    public Collection<ItemRequestDto> getAllRequests(
+            @RequestHeader(USER_ID_HEADER) Long userId
+    ) {
+        log.info("Get all requests userId={}", userId);
+        return itemRequestService.getAllRequests(userId);
+    }
+
+    @GetMapping("/{requestId}")
+    public ItemRequestDto getById(
+            @RequestHeader(USER_ID_HEADER) Long userId,
+            @PathVariable Long requestId
+    ) {
+        log.info("Get request by id={} userId={}", requestId, userId);
+        return itemRequestService.getById(userId, requestId);
+    }
+}
